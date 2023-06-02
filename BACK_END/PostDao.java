5482c4d96406914acee8de49fd67923ea0bc275a
KJM <kimjmini1122@gmail.com>
2023-06-02 17:47:11 +0900

    0602

--- BACK_END/PostDao.java
+++ BACK_END/PostDao.java
@@ -1,4 +1,4 @@
-package com.pcwk.ehr.post;
+package com.pcwk.ehr;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,8 +10,8 @@ import org.springframework.jdbc.core.RowMapper;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 
 public class PostDao {
 
@@ -33,165 +33,125 @@ public class PostDao {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
-	
-	
-	// 게시글 등록 = 생성
+	// 게시글 등록
 	public int add(final PostVO post) throws ClassNotFoundException, SQLException {
-		int flag = 0;// 등록 건수
-
+		int flag = 0; // 등록 건수
 		StringBuilder sb = new StringBuilder(200);
 		sb.append(" INSERT INTO post (  \n");
 		sb.append("     p_seq,          \n");
 		sb.append("     m_id,           \n");
 		sb.append("     p_title,        \n");
-		sb.append("     p_read_cnt,     \n");
-		sb.append("     p_atch_file_id, \n");
 		sb.append("     p_div,          \n");
 		sb.append("     p_contents,     \n");
-		sb.append("     p_reg_dt,       \n");
-		sb.append("     p_mod_dt,       \n");
-		sb.append("     p_like_cnt      \n");
+		sb.append("     p_reg_dt        \n");
 		sb.append(" ) VALUES (          \n");
 		sb.append("     ?,              \n");
 		sb.append("     ?,              \n");
 		sb.append("     ?,              \n");
 		sb.append("     ?,              \n");
 		sb.append("     ?,              \n");
-		sb.append("     ?,              \n");
-		sb.append("     ?,              \n");
-		sb.append("     ?,              \n");
-		sb.append("     ?,              \n");
-		sb.append("     ?               \n");
+		sb.append("    SYSDATE	        \n");
 		sb.append(" )                   \n");
+		LOG.debug("1. sql = \n" + sb.toString());
+		LOG.debug("2. param = \n" + post.toString());
+		// param
+		Object[] args = { post.getSeq(), post.getId(), post.getTitle(), post.getDiv(), post.getContents() };
+
+		// jdbcTemplate.update DML에 사용
+		flag = jdbcTemplate.update(sb.toString(), args);
+		LOG.debug("3. flag=" + flag);
+		return flag;
+	} // add()
+
+	// 게시글 수정
+	public int update(final PostVO post) throws ClassNotFoundException, SQLException {
+		int flag = 0;
+
+		StringBuilder sb = new StringBuilder(200);
+		sb.append(" UPDATE post                  \n");
+		sb.append(" SET                          \n");
+		sb.append("     p_title         = ?      \n");
+//		sb.append("     ,p_atch_file_id = ?      \n");
+		sb.append("     ,p_div          = ?      \n");
+		sb.append("     ,p_contents     = ?      \n");
+		sb.append("     ,p_mod_dt       = SYSDATE \n");
+		sb.append(" WHERE                        \n");
+		sb.append("     p_seq           = ?      \n");
 
 		LOG.debug("1. sql=\n" + sb.toString());
 		LOG.debug("2. param=\n" + post.toString());
 
 		// param
-		Object[] args = { 
-				post.getSeq(), 
-				post.getId(), 
-				post.getTitle(), 
-				post.getReadCnt(), 
-				post.getAtchFileId(),
-				post.getDiv(), 
-				post.getContents(), 
-				post.getRegDt(), 
-				post.getModDt(), 
-				post.getLikeCnt()
-				};
+		Object[] args = {
+				post.getTitle(),
+//				post.getAtchFileId(),
+				post.getDiv(),
+				post.getContents(),
+				post.getSeq() };
 
 		// jdbcTemplate.update DML에 사용
 		flag = jdbcTemplate.update(sb.toString(), args);
 		LOG.debug("3. flag=" + flag);
 
 		return flag;
-	}//게시글 등록 END
+	}// 게시글 수정 END
 
-
-	//게시글 수정	
-	public int update(final PostVO post) throws ClassNotFoundException, SQLException {
-		int flag = 0;	
-		
-	    	StringBuilder sb = new StringBuilder(200);
-	    	sb.append(" UPDATE post        \n");
-	    	sb.append(" SET p_seq =?,       \n");
-	        sb.append(" m_id =?,            \n");
-	        sb.append(" p_title =?,         \n");
-	        sb.append(" p_read_cnt =?,      \n");
-	        sb.append(" p_atch_file_id =?,  \n");
-	        sb.append(" p_div =?,           \n");
-	        sb.append(" p_contents =?,      \n");
-	        sb.append(" p_reg_dt =?,        \n");
-	        sb.append(" p_mod_dt =?,        \n");
-	        sb.append(" p_like_cnt =?       \n");
-	    	sb.append("WHERE p_seq = ?		\n");
-	        
-			LOG.debug("1. sql=\n" + sb.toString());
-			LOG.debug("2. param=\n" + post.toString());
-
-			// param
-			Object[] args = { 
-					post.getSeq(), 
-					post.getId(), 
-					post.getTitle(), 
-					post.getReadCnt(), 
-					post.getAtchFileId(),
-					post.getDiv(), 
-					post.getContents(), 
-					post.getRegDt(), 
-					post.getModDt(), 
-					post.getLikeCnt() };
-
-			// jdbcTemplate.update DML에 사용
-			flag = jdbcTemplate.update(sb.toString(), args);
-			LOG.debug("3. flag=" + flag);
-
-			return flag;
-	    }//게시글 수정	END
-	
 	// 게시글 조회
-	public PostVO get(PostVO postVO) throws ClassNotFoundException, SQLException {
+	public PostVO get(PostVO post) throws EmptyResultDataAccessException, ClassNotFoundException, SQLException {
 		PostVO outVO = null;
 
 		StringBuilder sb = new StringBuilder(200);
-		sb.append(" SELECT           	   \n");
-		sb.append("		 p_seq ,  		   \n");
-	    sb.append("		 m_id ,            \n");
-	    sb.append("		 p_title ,         \n");
-	    sb.append("		 p_read_cnt ,      \n");
-	    sb.append("		 p_atch_file_id ,  \n");
-	    sb.append("		 p_div ,           \n");
-	    sb.append("		 p_contents ,      \n");
-	    sb.append("		 p_reg_dt ,        \n");
-	    sb.append("		 p_mod_dt ,        \n");
-	    sb.append("		 p_like_cnt        \n");
-		sb.append(" FROM           		   \n");
-		sb.append("      post    		   \n");
-		sb.append(" WHERE 				   \n");
-		sb.append("		 p_seq = ?		   \n");
-		
+		sb.append(" SELECT           	  \n");
+		sb.append("		 p_seq,  		  \n");
+		sb.append("		 m_id,            \n");
+		sb.append("		 p_title,         \n");
+		sb.append("		 p_read_cnt,      \n");
+		sb.append("		 p_atch_file_id,  \n");
+		sb.append("		 p_div,           \n");
+		sb.append("		 p_contents,      \n");
+		sb.append("		 TO_CHAR(p_reg_dt, 'YYYY-MM-DD HH24MISS') p_reg_dt,        \n");
+		sb.append("		 TO_CHAR(p_mod_dt, 'YYYY-MM-DD HH24MISS') p_mod_dt,        \n");
+		sb.append("		 p_like_cnt       \n");
+		sb.append(" FROM           		  \n");
+		sb.append("      post          	  \n");
+		sb.append(" WHERE 			      \n");
+		sb.append("		 p_seq = ?	      \n");
 
 		LOG.debug("1. sql =\n" + sb.toString());
-		LOG.debug("2. param = " + outVO.toString());
+		LOG.debug("2. param = " + post.toString());
 
 		// param
-		Object[] args = { postVO.getSeq() };
-		
+		Object[] args = { post.getSeq() };
+
 		// jdbcTemplate
-		outVO = (PostVO) this.jdbcTemplate.queryForMap(sb.toString(), new RowMapper<PostVO>() {
-			
-		//-------------------------------------------------------
+		outVO = this.jdbcTemplate.queryForObject(sb.toString(), new RowMapper<PostVO>() {
+
 			@Override
 			public PostVO mapRow(ResultSet rs, int rowNum) throws SQLException {
 
 				PostVO out = new PostVO();
 
-				
-				out.setSeq        (rs.getString(" p_seq"));
-				out.setId         (rs.getString(" m_id"));
-				out.setTitle      (rs.getString(" p_title"));
-				out.setReadCnt    (rs.getInt(" p_read_cnt"));
-				out.setAtchFileId (rs.getString(" p_atch_file_id"));
-				out.setDiv        (rs.getInt(" p_div"));
-				out.setContents   (rs.getString(" p_contents "));
-				out.setRegDt      (rs.getString(" p_reg_dt"));
-				out.setModDt      (rs.getString(" p_mod_dt"));
-				out.setLikeCnt    (rs.getInt(" p_like_cnt"));
-
+				out.setSeq(rs.getString("p_seq"));
+				out.setId(rs.getString("m_id"));
+				out.setTitle(rs.getString("p_title"));
+				out.setReadCnt(rs.getInt("p_read_cnt"));
+				out.setAtchFileId(rs.getString("p_atch_file_id"));
+				out.setDiv(rs.getInt("p_div"));
+				out.setContents(rs.getString("p_contents"));
+				out.setRegDt(rs.getString("p_reg_dt"));
+				out.setModDt(rs.getString("p_mod_dt"));
+				out.setLikeCnt(rs.getInt("p_like_cnt"));
 
 				return out;
 			}
-		//-----------------------------------------------------------
-
 
 		}, args);
 
 		LOG.debug("3. outVO = " + outVO);
 
 		return outVO;
-	}// 게시글 조회 END	
-	
+	}// 게시글 조회 END
+
 	// 게시글 리스트
 	@SuppressWarnings("deprecation")
 	public List<PostVO> getALL(PostVO user) throws SQLException {
@@ -200,15 +160,15 @@ public class PostDao {
 		StringBuilder sb = new StringBuilder(200);
 		sb.append(" SELECT           	   \n");
 		sb.append("		t1.p_seq ,  		\n");
-	    sb.append("		t1.m_id ,          \n");
-	    sb.append("		t1.p_title ,       \n");
-	    sb.append("		t1.p_read_cnt ,    \n");
-	    sb.append("		t1.p_atch_file_id ,  \n");
-	    sb.append("		t1.p_div ,           \n");
-	    sb.append("		t1.p_contents ,      \n");
-	    sb.append("		t1.p_reg_dt ,        \n");
-	    sb.append("		t1.p_mod_dt ,        \n");
-	    sb.append("		t1.p_like_cnt        \n");
+		sb.append("		t1.m_id ,          \n");
+		sb.append("		t1.p_title ,       \n");
+		sb.append("		t1.p_read_cnt ,    \n");
+		sb.append("		t1.p_atch_file_id ,  \n");
+		sb.append("		t1.p_div ,           \n");
+		sb.append("		t1.p_contents ,      \n");
+		sb.append("		t1.p_reg_dt ,        \n");
+		sb.append("		t1.p_mod_dt ,        \n");
+		sb.append("		t1.p_like_cnt        \n");
 		sb.append(" FROM           		   \n");
 		sb.append("      post  t1   		 \n");
 		sb.append(" WHERE 				   \n");
@@ -229,16 +189,16 @@ public class PostDao {
 
 				PostVO out = new PostVO();
 
-				out.setSeq        (rs.getString(" p_seq"));
-				out.setId         (rs.getString(" m_id"));
-				out.setTitle      (rs.getString(" p_title"));
-				out.setReadCnt    (rs.getInt(" p_read_cnt"));
-				out.setAtchFileId (rs.getString(" p_atch_file_id"));
-				out.setDiv        (rs.getInt(" p_div"));
-				out.setContents   (rs.getString(" p_contents "));
-				out.setRegDt      (rs.getString(" p_reg_dt"));
-				out.setModDt      (rs.getString(" p_mod_dt"));
-				out.setLikeCnt    (rs.getInt(" p_like_cnt"));
+				out.setSeq(rs.getString(" p_seq"));
+				out.setId(rs.getString(" m_id"));
+				out.setTitle(rs.getString(" p_title"));
+				out.setReadCnt(rs.getInt(" p_read_cnt"));
+				out.setAtchFileId(rs.getString(" p_atch_file_id"));
+				out.setDiv(rs.getInt(" p_div"));
+				out.setContents(rs.getString(" p_contents "));
+				out.setRegDt(rs.getString(" p_reg_dt"));
+				out.setModDt(rs.getString(" p_mod_dt"));
+				out.setLikeCnt(rs.getInt(" p_like_cnt"));
 
 				return out;
 			}
@@ -247,33 +207,26 @@ public class PostDao {
 
 		return outList;
 	}// 게시글 리스트 END
-	
 
-
-	
-		//게시글 삭제
-		public int deleteOne(final PostVO post)throws SQLException{
+	// 게시글 삭제
+	public int deleteOne(final PostVO post) throws SQLException {
 		int flag = 0;
 
-		
-		StringBuilder  sb=new StringBuilder(50);
+		StringBuilder sb = new StringBuilder(50);
 		sb.append(" DELETE FROM post	 \n");
-		sb.append(" WHERE p_seq = ?     \n");
-		
-		LOG.debug("1. sql=\n"+sb.toString());
-		LOG.debug("2. param=\n"+post.toString());
-		
-		//param
-		Object[] args = {post.getSeq()};
-		
-		//jdbcTemplate.update DML에 사용
-		flag = jdbcTemplate.update(sb.toString(), args);
-		LOG.debug("3. flag="+flag);
-		
-		
-		return flag;
-	}//게시글 삭제 END
-	    
-	 
-}
+		sb.append(" WHERE p_seq = ?      \n");
 
+		LOG.debug("1. sql=\n" + sb.toString());
+		LOG.debug("2. param=\n" + post.toString());
+
+		// param
+		Object[] args = { post.getSeq() };
+
+		// jdbcTemplate.update DML에 사용
+		flag = jdbcTemplate.update(sb.toString(), args);
+		LOG.debug("3. flag=" + flag);
+
+		return flag;
+	}// 게시글 삭제 END
+
+}
--- BACK_END/PostDaoTest.java
+++ BACK_END/PostDaoTest.java
@@ -2,17 +2,26 @@ package com.pcwk.ehr;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hamcrest.core.IsSame;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.pcwk.ehr.PostDao;
+import com.pcwk.ehr.PostVO;
 
 @RunWith(SpringJUnit4ClassRunner.class) // 스프링 테스트 컨텍스 프레임워크의 JUnit확장 기능 지정
 @ContextConfiguration(locations = "/applicationContext.xml") // 테스트 컨텍스트가 자동으로 만들어줄 applicationContext 위치
@@ -41,9 +50,115 @@ public class PostDaoTest {
 		assertNotNull(context);
 		assertNotNull(dao);
 
-		postVO = new PostVO();
+		postVO = new PostVO("2023060100000077777777777777777777777777", "duck77", "오리는", 20, "꽥꽥");
 
-		post01 = new PostVO();
-	}
+		post01 = new PostVO("2023060100000077777777777777777777777777", "duck77", "오리는", 20, "꽤액");
 
-}
+	} // setUp()
+
+	@After
+	public void tearDown() {
+		LOG.debug("--------------");
+		LOG.debug("-tearDown-");
+		LOG.debug("--------------");
+	} // tearDown()
+
+	// 조회
+	@Test
+	@Ignore
+	public void get() throws EmptyResultDataAccessException, ClassNotFoundException, SQLException {
+		LOG.debug("=====================");
+		LOG.debug("=get()=");
+		LOG.debug("=====================");
+
+		// 1. 데이터 삭제
+		dao.deleteOne(post01);
+
+		// 2. 추가
+		dao.add(post01);
+
+		// 데이터 조회
+		PostVO outPost01 = dao.get(post01);
+
+		isSamePost(outPost01, post01);
+
+	} // get()
+
+	// 수정
+	@Test
+	//@Ignore
+	public void update() throws ClassNotFoundException, SQLException {
+		LOG.debug("=====================");
+		LOG.debug("=update()=");
+		LOG.debug("=====================");
+
+		// 1. 데이터 삭제
+		dao.deleteOne(post01);
+
+		// getCount 테스트
+		// assertEquals();
+
+		// 2. 추가
+		dao.add(post01);
+
+		// 3. 등록 데이터 조회
+		PostVO getVO = dao.get(post01);
+
+		// 3.1. 데이터 비교
+		isSamePost(post01, getVO);
+
+		// 4. 조회 데이터 수정
+		String upStr = "_U";
+		int upInt = 10;
+
+		// 문자열_U, 숫자 + 10
+		getVO.setTitle(getVO.getTitle() + upStr);
+//		getVO.setAtchFileId(getVO.getAtchFileId() + upStr);
+		getVO.setDiv(getVO.getDiv() + upInt);
+		getVO.setContents(getVO.getContents() + upStr);
+
+		// 5. 수정 수행
+		int flag = dao.update(getVO);
+		assertEquals(flag, 1);
+
+		// 6. 조회
+		PostVO fVO = dao.get(getVO);
+
+		// 6.1. 데이터 비교
+		isSamePost(fVO, getVO);
+
+	} // update()
+
+	public void isSamePost(PostVO outVO, PostVO postVO) {
+		assertEquals(outVO.getSeq(), postVO.getSeq());
+		assertEquals(outVO.getId(), postVO.getId());
+		assertEquals(outVO.getTitle(), postVO.getTitle());
+		assertEquals(outVO.getDiv(), postVO.getDiv());
+		assertEquals(outVO.getContents(), postVO.getContents());
+	} // isSamePost()
+
+	// 삭제
+	@Test
+	@Ignore
+	public void delete() throws ClassNotFoundException, SQLException {
+		LOG.debug("=====================");
+		LOG.debug("=delete()=");
+		LOG.debug("=====================");
+
+		dao.deleteOne(post01);
+
+	} // delete()
+
+	// 추가
+	@Test
+	@Ignore
+	public void add() throws ClassNotFoundException, SQLException {
+		LOG.debug("=====================");
+		LOG.debug("=add()=");
+		LOG.debug("=====================");
+
+		// 추가
+		dao.add(post01);
+	} // add()
+
+} // class end
