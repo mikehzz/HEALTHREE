d796342ca8fc73e0e96d42dd8a58c8d989297e10
주영 <110024599+Juyang22@users.noreply.github.com>
2023-06-05 17:45:20 +0900

    add targetWeight
    0605

--- BACK_END/MemberDao.java
+++ BACK_END/MemberDao.java
@@ -3,6 +3,9 @@ package com.pcwk.ehr;
 import java.sql.SQLException;
 
 public interface MemberDao {
+	
+	// 권장 칼로리
+	MemberVO myCalGoal(MemberVO member) throws ClassNotFoundException, SQLException;
 
 	// 건수 조회
 	int getCount(MemberVO member) throws SQLException;
@@ -18,5 +21,8 @@ public interface MemberDao {
 
 	// 회원가입
 	int add(MemberVO member) throws ClassNotFoundException, SQLException;
+	
+	// 목표체중 조회
+	double targetWeight(MemberVO member) throws SQLException;
 
 }
