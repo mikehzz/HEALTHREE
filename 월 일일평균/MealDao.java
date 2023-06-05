beb6db1fd52c36aebce5c30bfa6b511eb2e1be8a
원수연 <133944569+wonsooyeon1@users.noreply.github.com>
2023-06-05 17:58:22 +0900

    Add files via upload

---
+++ 월 일일평균/MealDao.java
@@ -0,0 +1,26 @@
+package com.pcwk.ehr;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface MealDao {
+
+	// 회원ID, 날짜까지 선택 후 조회
+	List<MealVO> getDivSeqFoodCode(MealVO meal) throws ClassNotFoundException, SQLException;
+
+	// 회원ID, 날짜, 식사구분까지 선택 후 조회
+	List<MealVO> getSeqFoodCode(MealVO meal) throws SQLException;
+
+	// 회원ID, 날짜, 식사구분, SEQ까지 선택 후 입력한 음식코드 조회 (단 건 조회)
+	MealVO getFoodCode(MealVO meal) throws ClassNotFoundException, SQLException;
+
+	// 삭제
+	int deleteOne(MealVO meal) throws SQLException;
+
+	// 추가
+	int add(MealVO meal) throws ClassNotFoundException, SQLException;
+	
+	// 월 일일 평균
+    List<MonthAvg> getMonthStats(String memberId, String mealType) throws SQLException;
+
+}
